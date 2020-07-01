2213c3713774e3f07c8377531f45afae41d64075
Anatolii Lishchynskyi <iamnotacake@github.com>
2020-07-01 19:23:31 +0300

    Add HTTP server, handle POST /upload as 501 Not Implemented

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use actix_web::{ post, App, HttpServer, Responder, web };
+
+#[post("/upload")]
+async fn upload() -> impl Responder {
+    web::HttpResponse::NotImplemented()
+}
+
 #[actix_rt::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(upload)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
