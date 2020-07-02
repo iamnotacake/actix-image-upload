64b74a1c63972c5c38cb348b0c689cee64b48c14
Anatolii Lishchynskyi <iamnotacake@github.com>
2020-07-02 21:15:05 +0300

    Allow configuration from a single place

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use actix_web::{ post, App, HttpServer, Responder, web };
 
+struct Config {
+    host: String,
+    port: u16,
+    tmp_dir: PathBuf,
+    uploads_dir: PathBuf,
+}
+
 #[post("/upload")]
 async fn upload() -> impl Responder {
     web::HttpResponse::NotImplemented()
@@ -7,11 +14,21 @@ async fn upload() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let config = Config {
+        host: "127.0.0.1".into(),
+        port: 8080,
+        tmp_dir: "/tmp".into(),
+        uploads_dir: "/tmp/uploads".into(),
+    };
+
+    tokio::fs::create_dir_all(&config.tmp_dir).await?;
+    tokio::fs::create_dir_all(&config.uploads_dir).await?;
+
     HttpServer::new(|| {
         App::new()
             .service(upload)
     })
-    .bind("127.0.0.1:8080")?
+    .bind((config.host.as_ref(), config.port))?
     .run()
     .await
 }
