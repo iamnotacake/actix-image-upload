47e31005d4a243b79b40fed7931274b3092c4591
Anatolii Lishchynskyi <iamnotacake@github.com>
2020-07-08 18:26:22 +0300

    Add guard to only allow POST multipart/form-data requests
    So the function upload_multipart will only handle uploaded files and not
    JSON+Base64-encoded or JSONs with URIs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use std::path::PathBuf;
-use actix_web::{ post, App, HttpServer, Responder, web };
+use actix_web::{ App, HttpServer, Responder, web, guard };
 use actix_multipart::Multipart;
 use tokio::stream::StreamExt;
-use actix_image_upload as lib;
 
 use actix_image_upload as lib;
 use lib::{ Config, UploadedFile };
 
-#[post("/upload")]
-async fn upload(mut multipart: Multipart, config: web::Data<Config>) -> impl Responder {
+async fn upload_multipart(mut multipart: Multipart, config: web::Data<Config>) -> impl Responder {
     if let Ok(Some(field)) = multipart.try_next().await {
         let extension = match lib::mime_type_to_extension(field.content_type().essence_str()) {
             Some(extension) => extension,
@@ -23,7 +20,7 @@ async fn upload(mut multipart: Multipart, config: web::Data<Config>) -> impl Res
 
         let res = lib::upload_image(field, &config.get_ref().uploads_dir, extension).await;
         match res {
-            Ok(lib::UploadedFile { id, path, thumbnail_path }) => {
+            Ok(UploadedFile { id, path, thumbnail_path }) => {
                 eprintln!(
                     "Upload succeed, id: {}, path: {}, thumbnail: {}",
                     id,
@@ -62,7 +59,18 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .data(config.clone())
-            .service(upload)
+            .service(
+                web::scope("/upload")
+                    .guard(guard::Post())
+                    .guard(guard::fn_guard(|req| {
+                        if let Some(content_type) = req.headers().get("content-type") {
+                            if let Ok(s) = content_type.to_str() {
+                                s.starts_with("multipart/form-data;")
+                            } else { false }
+                        } else { false }
+                    }))
+                    .route("", web::post().to(upload_multipart))
+            )
     })
     .bind((host.as_ref(), port))?
     .run()
